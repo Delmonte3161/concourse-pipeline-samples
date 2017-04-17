38902f53b1683ac65fb1f70ecd437ac79fc6cb89
Doug Saus <dsaus@cerner.com>
2017-04-17 12:45:19 -0500

    testing a change

--- blue-green-app-deployment/bgd-app/src/NumberBlackBox.js
+++ blue-green-app-deployment/bgd-app/src/NumberBlackBox.js
@@ -10,6 +10,7 @@ NumberBlackBox.prototype.getNumber = function() {
 NumberBlackBox.prototype.add = function(delta) {
   if ( isNaN(delta) ) { return NaN };
   this.myNumber += delta;
+  this.myNumber = this.myNumber;
   return this.myNumber;
 };
 
